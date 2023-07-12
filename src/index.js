c74db728100c83668d4097fcb1ed5033bfa8c9e1
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-07-12 16:22:28 +0300

    init2

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import GlobalCSS from 'styles/globalStyles';
 import { theme } from 'styles/theme';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalCSS />
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <GlobalCSS />
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
