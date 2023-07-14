15334e9693fba4d193b45d73f791b17931e2c37b
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-07-14 16:26:14 +0300

    add styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
-import { WelcomeTitle } from './pagesStyles/homePage.styled';
+import { WelcomePicture, WelcomeTitle } from './pagesStyles/homePage.styled';
+import img from '../service/images/phone-book.webp';
 
 const Home = () => {
-  return <WelcomeTitle>Welcome to the amazing Phonebook app!</WelcomeTitle>;
+  return (
+    <>
+      <WelcomeTitle>Welcome to the amazing Phonebook app!</WelcomeTitle>
+      <WelcomePicture src={img} alt="phonebook" />
+    </>
+  );
 };
 
 export default Home;
--- src/pages/pagesStyles/homePage.styled.js
+++ src/pages/pagesStyles/homePage.styled.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
 
 export const WelcomeTitle = styled.h1`
-  padding-top: 100px;
+  padding-top: 70px;
+  padding-bottom: 50px;
   font-size: ${({ theme }) => theme.fontSizes.large};
   text-align: center;
   color: ${({ theme }) => theme.colors.white};
   text-shadow: 1px 1px 2px black, 0 0 1em blue, 0 0 0.2em blue;
+`;
+
+export const WelcomePicture = styled.img`
+  margin: 0 auto;
+  width: 220px;
+  height: 220px;
 `;
